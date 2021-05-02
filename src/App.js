677143000ab73f629d33b99565cfa5f5312a01d4
chris <tiax.one@gmail.com>
2021-05-02 12:06:21 -0700

    Inital Check in hundred acre realty

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="HHR">
+      <header className="HHR-header">
+      </header>
+      <body>
+        <h1>Hundred ACRE Realty</h1>
+      </body>
+    </div>
+  );
+}
+
+export default App;
